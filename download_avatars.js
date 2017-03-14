2e23131d8158cb23954582d51c995bd4e70e1e9a
davidliang781205 <davidliang781205@gmail.com>
2017-03-14 00:04:51 +0000

    Added comments to download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -3,8 +3,6 @@ var fs = require('fs');
 
 const GITHUB_USER = "davidliang781205";
 const GITHUB_TOKEN = "32916607b43c4dfa5d5ef8d8c950114eb5219956";
-const repoOwner = process.argv[2];
-const repoName = process.argv[3];
 const BASE_URL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/';
 
 
@@ -13,11 +11,9 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 
-
-
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
 
+  // Check if any of the parameter is empty
   if (!repoOwner || !repoName){
     console.log('Please supply correct repoOwner and repoName.\nnode download_avatars.js <owner> <repo>');
   } else {
@@ -30,22 +26,18 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
     request(options, function(err, response, body) {
       if (err) throw err;
+      // Parsing output to Object
       var o = JSON.parse(body);
       o.forEach((obj) => {
+        // Build the filePath with login name
         var filePath = 'avatars/' + obj.login + '.jpg';
         cb(obj.avatar_url, filePath);
       });
     });
   }
-
 }
 
 
-getRepoContributors(repoOwner, repoName, downloadImageByURL);
-
-
-
-// var filePath =
 
 function downloadImageByURL(url, filePath) {
   // ...
@@ -55,12 +47,14 @@ function downloadImageByURL(url, filePath) {
       })
       .on('response', function (response) {
      })
+      // Print message upon successful download
       .on('end', (end) => console.log('Download completed!'))
       .pipe(fs.createWriteStream(filePath))
 }
 
 
 
+getRepoContributors(process.argv[2], process.argv[3], downloadImageByURL);
 
 
 
